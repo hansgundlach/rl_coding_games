f3c0aaf4751460b2befa852549fcc3a27a92f245
Hans Gundlach <hans.gundlach@gmail.com>
2025-09-30 19:59:11 -0400

    removed more materila it should all be in the extened code games folder

--- stuff_to_clean/agents/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Connect-X RL Agents Package."""
-
-from .base_agent import BaseAgent
-from .qwen_policy import QwenPolicyAgent
-from .random_agent import RandomAgent
-
-__all__ = ["BaseAgent", "QwenPolicyAgent", "RandomAgent"]
--- stuff_to_clean/game_environments/__init__.py
+++
@@ -1,1 +0,0 @@
-# Game environments package
